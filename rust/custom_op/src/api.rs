5c1b9ae25a0965179726519c993080557ea7ad74
Christian Bourjau <christian.bourjau@quantco.com>
2022-07-26 00:53:56 +0200

    Working state with safer api

--- rust/custom_op/src/api.rs
+++ rust/custom_op/src/api.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 #[derive(Debug)]
 pub struct Api<'s> {
-    api: &'s OrtApi
+    api: &'s OrtApi,
 }
 
 #[derive(Debug)]
@@ -13,7 +13,7 @@ pub struct KernelContext<'s> {
 
 #[derive(Debug)]
 pub struct KernelInfo<'s> {
-    inner: &'s OrtKernelInfo,
+    _kernel_info: &'s OrtKernelInfo,
 }
 
 // From context
@@ -27,7 +27,7 @@ pub struct Value<'s> {
 #[derive(Debug)]
 pub struct TensorTypeAndShapeInfo<'s> {
     api: &'s Api<'s>,
-    info: &'s OrtTensorTypeAndShapeInfo,
+    info: &'s mut OrtTensorTypeAndShapeInfo,
 }
 
 impl<'s> std::ops::Deref for Api<'s> {
@@ -40,94 +40,114 @@ impl<'s> std::ops::Deref for Api<'s> {
 
 impl<'s> Api<'s> {
     pub fn from_raw(api: *const OrtApi) -> Self {
-	unsafe{Self {api: &*api}}
+        unsafe { Self { api: &*api } }
     }
 }
 
 impl<'s> KernelContext<'s> {
     pub fn from_raw(api: &'s Api<'s>, context: *mut OrtKernelContext) -> Self {
-	dbg!(context);
-	unsafe{Self {api, context: &mut *context}}
+        unsafe {
+            Self {
+                api,
+                context: &mut *context,
+            }
+        }
     }
 
     pub fn get_input(&self, index: u64) -> Result<Value<'s>> {
-	let mut value: *const OrtValue = std::ptr::null();
-        status_to_result(
-	    unsafe{
-		self.api.KernelContext_GetInput.unwrap()(self.context, index, &mut value)
-	    }
-	)?;
-	if value.is_null() {
-	    status_to_result(
-		self.api.create_error_status(OrtErrorCode_ORT_FAIL, "Failed to get input")
-	    )?;
-	}
-	let value = unsafe{dbg!(&mut *(value as *mut OrtValue))};
-        Ok(Value{value, api: self.api})
+        let mut value: *const OrtValue = std::ptr::null();
+        status_to_result(unsafe {
+            self.api.KernelContext_GetInput.unwrap()(self.context, index, &mut value)
+        })?;
+        if value.is_null() {
+            status_to_result(
+                self.api
+                    .create_error_status(OrtErrorCode_ORT_FAIL, "Failed to get input"),
+            )?;
+        }
+        let value = unsafe { &mut *(value as *mut OrtValue) };
+        Ok(Value {
+            value,
+            api: self.api,
+        })
     }
 
     pub fn get_output(&mut self, index: u64, dimensions: &[i64]) -> Result<Value<'s>> {
         let mut value: *mut OrtValue = std::ptr::null_mut();
-        unsafe{
-	    status_to_result(
-		self.api.KernelContext_GetOutput.unwrap()(
-		self.context,
-		    index,
-		    dimensions.as_ptr(),
-		    dimensions.len() as u64,
-		    &mut value
-		)
-	    )?;
-	    Ok(Value{value: &mut *value, api: self.api})	
-	}
+        unsafe {
+            status_to_result(self.api.KernelContext_GetOutput.unwrap()(
+                self.context,
+                index,
+                dimensions.as_ptr(),
+                dimensions.len() as u64,
+                &mut value,
+            ))?;
+            Ok(Value {
+                value: &mut *value,
+                api: self.api,
+            })
+        }
     }
-    
+
+    #[allow(unused)]
     fn get_input_count(&self) -> Result<usize> {
-	unimplemented!()
+        unimplemented!()
     }
+
+    #[allow(unused)]
     fn get_output_count(&self) -> Result<usize> {
-	unimplemented!()
+        unimplemented!()
     }
 }
 
 impl<'s> KernelInfo<'s> {
+    #[allow(unused)]
     fn get_attribute<T>(&self, name: &str) -> Result<&T> {
-	unimplemented!()
+        unimplemented!()
     }
 
     // Not implemented for string?
+    #[allow(unused)]
     fn get_attribute_array<T>(&self, name: &str) -> Result<&[T]> {
-	unimplemented!()
-    }    
+        unimplemented!()
+    }
 }
 
 // Should be enum
+#[allow(unused)]
 type Type = ONNXType;
 
 impl<'s> Value<'s> {
     pub fn get_tensor_type_and_shape(&self) -> Result<TensorTypeAndShapeInfo<'s>> {
-	let mut info: *mut OrtTensorTypeAndShapeInfo = std::ptr::null_mut();
-        unsafe{
-	    self.api.GetTensorTypeAndShape.unwrap()(self.value, &mut info)
-	};
-	// TODO: ReleaseTensorTypeAndShapeInfo ought to be called somewhere!
-        Ok(TensorTypeAndShapeInfo{api: self.api, info: unsafe{&*info}})
+        let mut info: *mut OrtTensorTypeAndShapeInfo = std::ptr::null_mut();
+        unsafe {
+            self.api.GetTensorTypeAndShape.unwrap()(self.value, &mut info);
+            Ok(TensorTypeAndShapeInfo {
+                api: self.api,
+                info: &mut *info,
+            })
+        }
     }
 
+    #[allow(unused)]
     pub fn get_value_type(&self) -> Result<Type> {
-	unimplemented!()
+        unimplemented!()
     }
 
-    pub fn get_tensor_mutable_data<T>(&mut self) -> Result<&'s mut [T]> {
-        let element_count = {
-	    let info = self.get_tensor_type_and_shape()?;
-	    info.get_tensor_shape_element_count()?
-	};
+    /// Get mutable tensor data associated to this `Value`.
+    ///
+    /// Calling this function repeatedly hands out aliasing mutable slices!
+    pub unsafe fn get_tensor_data_mut<T>(&mut self) -> Result<&'s mut [T]> {
+        let element_count = self
+            .get_tensor_type_and_shape()?
+            .get_tensor_shape_element_count()?;
+
         let mut ptr: *mut _ = std::ptr::null_mut();
-        unsafe{
-	    self.api.GetTensorMutableData.unwrap()(self.value, &mut ptr);
-	    Ok(std::slice::from_raw_parts_mut(ptr as *mut T, element_count as usize))
-	}
+        self.api.GetTensorMutableData.unwrap()(self.value, &mut ptr);
+        Ok(std::slice::from_raw_parts_mut(
+            ptr as *mut T,
+            element_count as usize,
+        ))
     }
 
     // /// Not clear what that is...
@@ -139,40 +159,38 @@ impl<'s> Value<'s> {
 impl<'s> TensorTypeAndShapeInfo<'s> {
     pub fn get_dimensions(&self) -> Result<Vec<i64>> {
         let mut n_dim = 0;
-        unsafe{
-	    self.api.GetDimensionsCount.unwrap()(self.info, &mut n_dim)
-	};
-	let mut out = Vec::with_capacity(n_dim as usize);
-        unsafe{
-	    self.api.GetDimensions.unwrap()(self.info, out.as_mut_ptr(), n_dim);
-	    out.set_len(n_dim as usize);
-	}
-	Ok(out)
+        unsafe { self.api.GetDimensionsCount.unwrap()(self.info, &mut n_dim) };
+        let mut out = Vec::with_capacity(n_dim as usize);
+        unsafe {
+            self.api.GetDimensions.unwrap()(self.info, out.as_mut_ptr(), n_dim);
+            out.set_len(n_dim as usize);
+        }
+        Ok(out)
     }
 
     fn get_tensor_shape_element_count(&self) -> Result<u64> {
-	let mut element_count = 0;
-	status_to_result(
-	    unsafe {
-		self.api.GetTensorShapeElementCount.unwrap()(self.info, &mut element_count)
-	    }
-	)?;
-	Ok(element_count)
+        let mut element_count = 0;
+        status_to_result(unsafe {
+            self.api.GetTensorShapeElementCount.unwrap()(self.info, &mut element_count)
+        })?;
+        Ok(element_count)
     }
 
+    #[allow(unused)]
     fn get_tensor_element_type(&self) -> Result<ONNXTensorElementDataType> {
-	unimplemented!()
+        unimplemented!()
     }
     //...
 }
 
 impl<'s> Drop for TensorTypeAndShapeInfo<'s> {
     fn drop(&mut self) {
+        unsafe { self.api.ReleaseTensorTypeAndShapeInfo.unwrap()(&mut *self.info) }
     }
 }
 
 impl<'s> Drop for Value<'s> {
     fn drop(&mut self) {
+        // This is unused in the official example and crashes if used...
     }
 }
-
--- rust/custom_op/src/lib.rs
+++ rust/custom_op/src/lib.rs
@@ -20,9 +20,9 @@ fn str_to_c_char_ptr(s: &str) -> *const c_char {
 ///A null pointer is mapped to the `Ok(())`.
 fn status_to_result(ptr: OrtStatusPtr) -> Result<()> {
     if ptr == std::ptr::null_mut() {
-	Ok(())
+        Ok(())
     } else {
-	Err(ptr)
+        Err(ptr)
     }
 }
 
@@ -36,20 +36,15 @@ impl OrtApi {
         &self,
         domain: &str,
         options: &mut OrtSessionOptions,
-    ) -> *mut OrtCustomOpDomain {
+    ) -> Result<*mut OrtCustomOpDomain> {
         let fun_ptr = self.CreateCustomOpDomain.unwrap();
         let mut domain_ptr: *mut OrtCustomOpDomain = std::ptr::null_mut();
         let c_op_domain = str_to_c_char_ptr(domain);
-        let status = unsafe { fun_ptr(c_op_domain, &mut domain_ptr) };
-        if status != std::ptr::null_mut() {
-            panic!()
-            // return status;
-        }
-        // Somehow, one must ensure that the domain stays alive?
-        // ...
-        dbg!(domain_ptr);
-        dbg!(unsafe { self.AddCustomOpDomain.unwrap()(options, domain_ptr) });
-        domain_ptr
+	unsafe {
+            status_to_result(fun_ptr(c_op_domain, &mut domain_ptr))?;
+            status_to_result(self.AddCustomOpDomain.unwrap()(options, domain_ptr))?;
+	}
+        Ok(domain_ptr)
     }
 
     fn add_op_to_domain(&self, domain: *mut OrtCustomOpDomain) -> Result<()> {
@@ -59,88 +54,36 @@ impl OrtApi {
     }
 }
 
-impl OrtKernelContext {
-    unsafe fn get_input(&self, api: &OrtApi, index: u64) -> Result<*mut OrtValue> {
-        let mut value: *const OrtValue = std::ptr::null();
-        status_to_result(api.KernelContext_GetInput.unwrap()(self, index, &mut value))?;
-	if value == std::ptr::null() {
-	    status_to_result(
-		api.create_error_status(OrtErrorCode_ORT_FAIL, "Failed to get input")
-	    )?;
-	}
-        Ok(value as *mut _)
-    }
-
-    unsafe fn get_output(
-        &mut self,
-        api: &OrtApi,
-        index: u64,
-        shape_like: &OrtTensorTypeAndShapeInfo,
-    ) -> *mut OrtValue {
-        let dims = {
-            let mut n_dim = 0;
-            api.GetDimensionsCount.unwrap()(shape_like, &mut n_dim);
-	    let mut out = Vec::with_capacity(n_dim as usize);
-            dbg!(api.GetDimensions.unwrap()(shape_like, out.as_mut_ptr(), n_dim));
-	    out.set_len(n_dim as usize);
-            dbg!(out)
-        };
-
-        let mut value: *mut OrtValue = std::ptr::null_mut();
-        let _status = dbg!(api.KernelContext_GetOutput.unwrap()(
-            self,
-            index,
-            dims.as_ptr(),
-            dims.len() as u64,
-            &mut value
-        ));
-        dbg!(value) as *mut _
-    }
-}
-
-impl OrtValue {
-    unsafe fn get_type_and_shape_info(&self, api: &OrtApi) -> *mut OrtTensorTypeAndShapeInfo {
-        let mut info: *mut OrtTensorTypeAndShapeInfo = std::ptr::null_mut();
-        api.GetTensorTypeAndShape.unwrap()(self as *const _, &mut info);
-	// TODO: ReleaseTensorTypeAndShapeInfo ought to be called somewhere!
-        dbg!(info)
-    }
-
-    unsafe fn get_tensor_mutable_data<T>(&mut self, api: &OrtApi) -> &mut [T] {
-        let info = self.get_type_and_shape_info(api);
-        let mut element_count = 0;
-        api.GetTensorShapeElementCount.unwrap()(info, &mut element_count);
-
-        let mut ptr: *mut _ = std::ptr::null_mut();
-        api.GetTensorMutableData.unwrap()(self, &mut ptr);
-
-        std::slice::from_raw_parts_mut(ptr as *mut T, element_count as usize)
-    }
-}
-
 #[derive(Debug)]
 struct Kernel {
-    op: *const OrtCustomOp,
+    _op: *const OrtCustomOp,
     api: *const OrtApi,
-    info: *const OrtKernelInfo,
+    _info: *const OrtKernelInfo,
 }
 
-unsafe extern "C" fn create_kernel(
+extern "C" fn create_kernel(
     op: *const OrtCustomOp,
     api: *const OrtApi,
     info: *const OrtKernelInfo,
 ) -> *mut c_void {
-    let kernel = Kernel { op, api, info };
+    let kernel = Kernel {
+        _op: op,
+        api,
+        _info: info,
+    };
     Box::leak(Box::new(kernel)) as *mut _ as *mut c_void
 }
 
 unsafe extern "C" fn get_execution_provider_type(
     _op: *const OrtCustomOp,
 ) -> *const ::std::os::raw::c_char {
-    dbg!("provider");
     CString::new("CPUExecutionProvider").unwrap().into_raw()
 }
 
+unsafe extern "C" fn kernel_destroy(op_kernel: *mut c_void) {
+    Box::from_raw(op_kernel as *mut Kernel);
+    dbg!("kernel_destroy");
+}
 
 mod op_one {
     use super::*;
@@ -163,7 +106,7 @@ mod op_one {
     };
 
     extern "C" fn get_name(_op: *const OrtCustomOp) -> *const c_char {
-        dbg!(str_to_c_char_ptr("CustomOpOne"))
+        str_to_c_char_ptr("CustomOpOne")
     }
 
     unsafe extern "C" fn get_input_type(
@@ -192,35 +135,31 @@ mod op_one {
         1
     }
 
-    unsafe extern "C" fn kernel_compute(
-        op_kernel: *mut c_void,
-        context: *mut OrtKernelContext,
-    ) {
+    unsafe extern "C" fn kernel_compute(op_kernel: *mut c_void, context: *mut OrtKernelContext) {
         let kernel: &Kernel = &*(op_kernel as *const _);
-	let api = Api::from_raw(kernel.api);
-	let mut context = KernelContext::from_raw(&api, context);
-        // let api = &*kernel.api;
-        // let context = &mut *context;
+        let api = Api::from_raw(kernel.api);
+        let mut context = KernelContext::from_raw(&api, context);
+
         let (dims_x, array_x) = {
-	    let mut value = context.get_input(0).unwrap();
-	    let array = value.get_tensor_mutable_data::<f32>().unwrap();
-	    let info = value.get_tensor_type_and_shape().unwrap();
-	    let dims = info.get_dimensions().unwrap();
-            dbg!(dims, array)
+            let mut value = context.get_input(0).unwrap();
+            let array = value.get_tensor_data_mut::<f32>().unwrap();
+            let info = value.get_tensor_type_and_shape().unwrap();
+            let dims = info.get_dimensions().unwrap();
+            dbg!((dims, array))
         };
         let array_y = {
             let mut value = context.get_input(1).unwrap();
-            let array = value.get_tensor_mutable_data::<f32>().unwrap();
+            let array = value.get_tensor_data_mut::<f32>().unwrap();
             dbg!(array)
         };
         let array_z = {
-            let mut value = dbg!(context.get_output(0, &dims_x)).unwrap();
-            let array = value.get_tensor_mutable_data::<f32>().unwrap();
-            dbg!(array)
+            let mut value = context.get_output(0, &dims_x).unwrap();
+            let array = value.get_tensor_data_mut::<f32>().unwrap();
+            array
         };
-	for ((x, y), z) in array_x.iter().zip(array_y.iter()).zip(array_z.iter_mut()) {
-	    *z  = *x + *y;
-	}
+        for ((x, y), z) in array_x.iter().zip(array_y.iter()).zip(array_z.iter_mut()) {
+            *z = *x + *y;
+        }
         dbg!("kernel_compute-one");
     }
     unsafe extern "C" fn get_input_characteristic(
@@ -236,10 +175,6 @@ mod op_one {
     ) -> OrtCustomOpInputOutputCharacteristic {
         dbg!("get_out_characteristic");
         unimplemented!()
-    }
-
-    unsafe extern "C" fn kernel_destroy(op_kernel: *mut c_void) {
-        dbg!("kernel_destroy");
     }
 }
 
@@ -264,7 +199,7 @@ mod op_two {
     };
 
     extern "C" fn get_name(_op: *const OrtCustomOp) -> *const c_char {
-        dbg!(str_to_c_char_ptr("CustomOpTwo"))
+        str_to_c_char_ptr("CustomOpTwo")
     }
 
     unsafe extern "C" fn get_input_type(
@@ -288,30 +223,26 @@ mod op_two {
     unsafe extern "C" fn get_output_type_count(_op: *const OrtCustomOp) -> size_t {
         1
     }
-    unsafe extern "C" fn kernel_compute(
-        op_kernel: *mut c_void,
-        context: *mut OrtKernelContext,
-    ) {
-	dbg!("begin compute two");
+    unsafe extern "C" fn kernel_compute(op_kernel: *mut c_void, context: *mut OrtKernelContext) {
         let kernel: &Kernel = &*(op_kernel as *const _);
-        let api = &*kernel.api;
-        let context = &mut *context;
+        let api = Api::from_raw(kernel.api);
+        let mut context = KernelContext::from_raw(&api, context);
 
-        let (info_x, array_x) = {
-            let value = dbg!(context.get_input(api, 0)).unwrap();
-            let array = (*value).get_tensor_mutable_data::<f32>(api);
-	    let info = (*value).get_type_and_shape_info(api);
-            dbg!(info, array)
+        let (dims_x, array_x) = {
+            let mut value = context.get_input(0).unwrap();
+            let array = value.get_tensor_data_mut::<f32>().unwrap();
+            let info = value.get_tensor_type_and_shape().unwrap();
+            let dims = info.get_dimensions().unwrap();
+            (dims, array)
         };
         let array_z = {
-            let value = dbg!(context.get_output(api, 0, &*info_x));
-            let array = (*value).get_tensor_mutable_data::<i32>(api);
-            dbg!(array)
+            let mut value = context.get_output(0, &dims_x).unwrap();
+            let array = value.get_tensor_data_mut::<i32>().unwrap();
+            array
         };
-	for (x, z) in array_x.iter().zip(array_z.iter_mut()) {
-	    *z  = x.round() as i32
-	}	
-        dbg!("kernel_compute-two");
+        for (x, z) in array_x.into_iter().zip(array_z.iter_mut()) {
+            *z = x.round() as i32
+        }
     }
     unsafe extern "C" fn get_input_characteristic(
         _op: *const OrtCustomOp,
@@ -326,9 +257,6 @@ mod op_two {
     ) -> OrtCustomOpInputOutputCharacteristic {
         dbg!("get_out_characteristic");
         unimplemented!()
-    }
-    unsafe extern "C" fn kernel_destroy(op_kernel: *mut c_void) {
-        dbg!("kernel_destroy");
     }
 }
 
@@ -351,22 +279,12 @@ pub extern "C" fn RegisterCustomOps(
     // all sessions using the passed in session options are destroyed,
     // or if an error occurs and it is non null.
     let api = unsafe { &*api_base.GetApi.unwrap()(12) };
-    let domain_ptr = api.create_custom_op_domain("test.customop", options);
-    match api.add_op_to_domain(domain_ptr) {
-	Ok(_) => std::ptr::null_mut(),
-	Err(status) => status,
+    let status = api.create_custom_op_domain("test.customop", options)
+	.and_then(|domain_ptr| {
+	    api.add_op_to_domain(domain_ptr)
+	});
+    match status {
+        Ok(_) => std::ptr::null_mut(),
+        Err(status) => status,
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn loopy_test() {
-//         let api = unsafe{*(*OrtGetApiBase()).GetApi.unwrap()(12)};
-// 	let mut so_ptr: *mut OrtSessionOptions = std::ptr::null_mut();
-// 	unsafe{api.CreateSessionOptions.unwrap()(&mut so_ptr)};
-// 	unsafe{api.RegisterCustomOpsLibrary.unwrap()(so_ptr, str_to_c_char_ptr(""), std::ptr::null_mut())};
-//     }
-
-// }
