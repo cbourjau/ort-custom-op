c476a3c1a034a9b88e531906354d27b16f476491
Christian Bourjau <christian.bourjau@quantco.com>
2023-08-31 14:15:27 +0200

    Support tensor attributes

--- example/src/attr_showcase.rs
+++ example/src/attr_showcase.rs
@@ -9,6 +9,7 @@ pub struct AttrShowcase {
     int_attr: i64,
     string_attr: String,
 
+    _u8_tensor: ArrayD<u8>,
     _floats_attr: Vec<f32>,
     _ints_attr: Vec<i64>,
 }
@@ -24,6 +25,7 @@ impl CustomOp for AttrShowcase {
             float_attr: info.get_attribute_f32("float_attr").unwrap(),
             int_attr: info.get_attribute_i64("int_attr").unwrap(),
             string_attr: info.get_attribute_string("string_attr").unwrap(),
+            _u8_tensor: info.get_attribute_tensor("u8_tensor").unwrap().to_owned(),
             _floats_attr: info.get_attribute_f32s("floats_attr").unwrap(),
             _ints_attr: info.get_attribute_i64s("ints_attr").unwrap(),
         }
