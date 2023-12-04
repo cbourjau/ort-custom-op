65dc4465be82471d8ba8596dc2cebc22a4733627
Christian Bourjau <cbourjau@users.noreply.github.com>
2023-12-04 11:07:38 +0100

    Make the Add example generic over the input/output type (#11)

--- example/src/add.rs
+++ example/src/add.rs
@@ -1,29 +1,36 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, marker::PhantomData, ops::Add};
 
 use ndarray::{ArrayD, ArrayViewD};
 
 use ort_custom_op::prelude::*;
 
 /// A custom operator which adds its two inputs
-pub struct CustomAdd;
+pub struct CustomAdd<T> {
+    ty: PhantomData<T>,
+}
 
-impl CustomOp for CustomAdd {
+impl<'add, T> CustomOp for CustomAdd<T>
+where
+    T: 'static + Add<T, Output = T> + Clone,
+    for<'s> (ArrayViewD<'s, T>, ArrayViewD<'s, T>): Inputs<'s>,
+    (ArrayD<T>,): Outputs,
+{
     type KernelCreateError = Infallible;
     type ComputeError = Infallible;
 
     const NAME: &'static str = "CustomAdd";
 
-    type OpInputs<'s> = (ArrayViewD<'s, f32>, ArrayViewD<'s, f32>);
-    type OpOutputs = (ArrayD<f32>,);
+    type OpInputs<'s> = (ArrayViewD<'s, T>, ArrayViewD<'s, T>);
+    type OpOutputs = (ArrayD<T>,);
 
     fn kernel_create(_info: &KernelInfo) -> Result<Self, Self::KernelCreateError> {
-        Ok(CustomAdd)
+        Ok(CustomAdd { ty: PhantomData })
     }
 
     fn kernel_compute(
         &self,
         (array_x, array_y): Self::OpInputs<'_>,
-    ) -> Result<Self::OpOutputs, Self::KernelCreateError> {
+    ) -> Result<Self::OpOutputs, Self::ComputeError> {
         Ok((&array_x + &array_y,))
     }
 }
